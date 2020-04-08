1212477cb82a283e5a58384c86eabb3fed89bd08
lalaantika <sharmal0707@gmail.com>
2020-04-07 22:31:12 -0400

    made the mobilenavbar working

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function openForm() {
-  document.getElementById("myForm").style.display = "block";
+    document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+    document.getElementById("myForm").style.display = "none";
 }
- 
+
+//sidebar
+function openNav() {
+    document.getElementById("mySidenav").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+}
+//alert window
+function myFunction() {
+    alert("Thanks for getting start with us");
+}
